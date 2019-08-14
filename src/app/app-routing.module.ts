7d3c3d3e010c2b5c948a774d3c8679afef2f6f16
2355925603@qq.com <2355925603@qq.com>
2019-08-14 18:16:45 +0800

    zboke

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app-home',
+    component: HomeComponent
+  },
+  { path: '',
+    redirectTo: '/app-home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
